4c7ad2c900c2e153dc7d95d2605ba55f0cc7978d
Oğuz Özözgen <ouz.zzgen@yandex.com>
2022-08-17 02:20:57 +0300

    Add files via upload

---
+++ include/wxImage.h
@@ -0,0 +1,16 @@
+#ifndef WXIMAGE_H
+#define WXIMAGE_H
+
+
+class wxImage
+{
+    public:
+        wxImage() {}
+        virtual ~wxImage() {}
+
+    protected:
+
+    private:
+};
+
+#endif // WXIMAGE_H
